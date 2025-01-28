11b8a189b59d0d08f163132c059ab7685a3c1c24
tokkyo54 <tokkyo54@gmail.com>
2025-01-28 17:00:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["Watching Horizon City Roleplay", "Watching All Members"];
+const statusMessages = ["Watching Horizon City Roleplay", "Watching 1K+ Members"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
